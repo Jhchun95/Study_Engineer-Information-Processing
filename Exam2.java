e3e8381fa66924ee8fb2acd8e5c340fdb0f095b1
Jhchun95 <75196751+Jhchun95@users.noreply.github.com>
2021-04-09 21:21:40 +0900

    Add files via upload

---
+++ Exam2.java
@@ -0,0 +1,22 @@
+package mydolist;
+
+class Parent {
+	int compute(int num) {
+		if(num<=1) return num;
+		return compute(num-1) + compute(num-2);
+	}
+}
+
+class Child extends Parent {
+	int compute(int num) {
+		if(num<=1) return num;
+		return compute(num-1) + compute(num-3);
+	}
+}
+
+public class Exam2 {
+	public static void main(String[] args) {
+		Parent obj = new Child();
+		System.out.println(obj.compute(4));
+	}
+}
