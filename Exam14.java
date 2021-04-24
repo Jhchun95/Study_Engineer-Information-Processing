07e5bd9f463322a27d71703000e5b9e31e6761fd
Jhchun95 <75196751+Jhchun95@users.noreply.github.com>
2021-04-24 14:43:53 +0900

    Add files via upload

---
+++ Exam14.java
@@ -0,0 +1,23 @@
+package mydolist;
+
+class A {
+	int a;
+	public A(int a) {
+		this.a = a;
+	}
+	void display() {
+		System.out.println("a=" + a);
+	}
+}
+class B extends A {
+	public B(int a) {
+		super(a);
+		super.display();
+	}
+}
+public class Exam14 {
+	public static void main(String[] args) {
+		B obj = new B(10);
+	}
+
+}
