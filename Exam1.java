2148af87ffa09aad06b563bd5c8592c24c3e70f5
Jhchun95 <75196751+Jhchun95@users.noreply.github.com>
2021-04-29 22:40:05 +0900

    Update Exam1.java

--- Exam1.java
+++ Exam1.java
@@ -11,8 +11,15 @@ abstract class Vehicle {
 	}
 }
 
+// 추상메소드 : 자식 클래스에서 재정의해야만 사용할 수 있는 메소드
+//	ex) Car => 트럭, 승용차, 버스 등등
+// 오버라이딩 : 조상 클래스부터 상속 받은 메소드의 내용을 변경하는 것
+// 오버로딩 : 한 클래스 내에 같은 이름의 메소드를 여러 개 작성하는 것
+
+
 class Car extends Vehicle {
 	private String name;
+	
 	public Car(String val) {
 		name = super.name = val;
 	}
@@ -26,6 +33,7 @@ class Car extends Vehicle {
 
 public class Exam1 {
 	public static void main(String[] args) {
+		// 객체 선언
 		Vehicle obj = new Car("Spark");
 		System.out.print(obj.getName());
 	}
