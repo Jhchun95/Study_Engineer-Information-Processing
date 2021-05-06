0884ba670cdefdddfe6835ab95a5742f90dedcf9
Jhchun95 <75196751+Jhchun95@users.noreply.github.com>
2021-05-06 23:43:51 +0900

    Update Exam4.java

--- Exam4.java
+++ Exam4.java
@@ -5,14 +5,20 @@ import java.util.Scanner;
 public class Exam4 {
 	public static void main(String[] args) {
 		int i;
+		// 키보드 입력값을 받는 Scanner
 		Scanner s = new Scanner(System.in);
+		// 정수형 Int
 		i = s.nextInt();
+		// 함수
 		recursive(i);
+		// 프로그램을 종료한다.
 		s.close();
 	}
 	
 	static int recursive(int n) {
+		// 정수형 i
 		int i;
+		// 재귀함수 호출하게 도와주는 
 		if(n<1)
 			return 2;
 		else {
