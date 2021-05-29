6c620fd85f285017cdbfd848cfce1ac83c6ef50a
Jhchun95 <75196751+Jhchun95@users.noreply.github.com>
2021-05-29 13:46:03 +0900

    Update Exam6.java

--- Exam6.java
+++ Exam6.java
@@ -1,12 +1,16 @@
 package mydolist;
 
+// 복습
+// 부모 클래스
 class Parents {
 	int compute(int num) {
+		// 오버라이딩을 통해서 이것으로 작동
 		if(num<=1) return num;
 		return compute(num-1) + compute(num-2);
 	}
 }
 
+// 부모클래스에서 상속받는 자식 클래스
 class Childs extends Parents{
 	int compute(int num)  {
 		if(num<=1) return num;
@@ -16,7 +20,9 @@ class Childs extends Parents{
 
 public class Exam6 {
 	public static void main(String[] args) {
+		// 객체 생성
 		Parents obj = new Childs();
+		// 결과 출력
 		System.out.print(obj.compute(4));
 	}
 }
