7777179f866a663698ae192e07f90695372f45cf
Jhchun95 <75196751+Jhchun95@users.noreply.github.com>
2021-05-30 23:05:15 +0900

    Update Etc7.java

--- Etc7.java
+++ Etc7.java
@@ -7,19 +7,23 @@ public class Etc7 {
 	// static 멤버 간에는 서로 직접 접근이 가능하다.
 	class InstanceInner {
 		int iv = 100;
+		// 에러! static 변수를 선언할 수 없다.
 //		static int cv = 100;
 		final static int CONST = 100;
 	}
 	
 	static class StaticInner {
 		int iv = 200;
+		// static 클래스만 static 멤버를 정의할 수 있다.
 		static int cv = 200;
 	}
 	
 	void myMethod() {
 		class LocalInner{
 			int iv = 300;
+			// 에러! static 변수를 선언할 수 없다.
 	//		static int cv = 300;
+			// final static은 상수이므로 허용한다.
 			final static int CONST = 300;
 		}
 	}
