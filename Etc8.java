a7906f8b94154fbd64d5ba5b651f9c15e5ce9d67
Jhchun95 <75196751+Jhchun95@users.noreply.github.com>
2021-05-31 22:40:09 +0900

    Add files via upload

---
+++ Etc8.java
@@ -0,0 +1,39 @@
+package mydolist;
+
+public class Etc8 {
+	public static void main(String[] args) {
+		// 길이가 3인 Tv객체 배열
+		Tv[] tvArr = new Tv[3];
+
+		// Tv 객체를 생성해서 Tv 객체 배열의 각 요소에 저장
+		for(int i=0; i< tvArr.length; i++) {
+			tvArr[i] = new Tv();
+			// tvArr[i]의 channel에 i+10을 저장
+			tvArr[i].channel = i + 10;
+		}
+		
+		for(int i=0; i < tvArr.length; i++) {
+			tvArr[i].channelUp();
+			System.out.printf("tvArr[%d].channel = %d%n", i, tvArr[i].channel);
+		}
+		
+	}
+
+}
+
+class Tv {
+	String color;
+	boolean power;
+	int channel;
+	
+	void power() {
+		power =! power;
+	}
+	void channelUp() {
+		++channel;
+	}
+	void channelDown() {
+		--channel;
+	}
+	
+}
