632e0a010eb311486499538e771f9e6d75cc01ea
Jhchun95 <75196751+Jhchun95@users.noreply.github.com>
2021-06-01 22:24:08 +0900

    Update Exam14.java

--- Exam14.java
+++ Exam14.java
@@ -4,15 +4,20 @@ package mydolist;
 // 부모 A
 class A {
 	int a;
+	// public A
 	public A(int a) {
 		this.a = a;
 	}
+	// display()
 	void display() {
+		// 출력
 		System.out.println("a=" + a);
 	}
 }
 // 자식 B
+// 상속
 class B extends A {
+	// class A로 이동
 	public B(int a) {
 		super(a);
 		super.display();
@@ -20,6 +25,7 @@ class B extends A {
 }
 public class Exam14 {
 	public static void main(String[] args) {
+		// 객체 생성
 		B obj = new B(10);
 	}
 
