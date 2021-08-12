b73bf5cbb4440f9d96bab0241796eed8ca8e0319
Jhchun95 <75196751+Jhchun95@users.noreply.github.com>
2021-08-12 23:44:59 +0900

    Update Etc11.java

--- Etc11.java
+++ Etc11.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class Etc11 {
 
 	public static void main(String[] args) {
+		// 키보드 입력
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 		char c = 'G';
 		
+		// 조건문
 		if(a>10 && b<10) {
 			if(a-b < 5 || a>15)
 				c='X';
@@ -20,6 +22,7 @@ public class Etc11 {
 		}
 		else
 			c = 'Z';
+		// 결과 
 		System.out.printf("%c", c);
 		scan.close();
 	}
