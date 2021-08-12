406cbb6618985ef4eb51ba46a14b48d1727e0426
Jhchun95 <75196751+Jhchun95@users.noreply.github.com>
2021-08-13 00:28:01 +0900

    Update Etc11.java

--- Etc11.java
+++ Etc11.java
@@ -5,14 +5,18 @@ import java.util.Scanner;
 public class Etc11 {
 
 	public static void main(String[] args) {
-		// 키보드 입력
+		// 키보드로 값을 임의로 입력하는 코드들
 		Scanner scan = new Scanner(System.in);
+		// a와 b를 정수로 받는다.
 		int a = scan.nextInt();
 		int b = scan.nextInt();
+		// char형 변수 c는 G다.
 		char c = 'G';
 		
 		// 조건문
+		// && : 
 		if(a>10 && b<10) {
+			// || : 또는
 			if(a-b < 5 || a>15)
 				c='X';
 		}
