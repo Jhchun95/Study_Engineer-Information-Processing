b3d2225e0013379e8e07faf1ba4f4d8a132d9822
Jhchun95 <75196751+Jhchun95@users.noreply.github.com>
2021-09-05 23:20:53 +0900

    Update Etc3.java

--- Etc3.java
+++ Etc3.java
@@ -6,6 +6,8 @@ public class Etc3 {
 		
 		for(int i=0; i< src.length(); i++) {
 			// char형 숙지하기
+			// char는 character의 약자로 문자 1개를 저장하는 자료형이다.
+			// https://imasoftwareengineer.tistory.com/46 괜찮으니 읽어보면서 정리하기
 			// src의 i번째 문자를 ch에 저장
 			char ch = src.charAt(i);
 			System.out.println("src.charAt("+i+":" + ch);
