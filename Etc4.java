6677d89568677698ef34528e8303c2d70cf39a64
Jhchun95 <75196751+Jhchun95@users.noreply.github.com>
2021-09-11 22:32:06 +0900

    Update Etc4.java

--- Etc4.java
+++ Etc4.java
@@ -13,10 +13,11 @@ public class Etc4 {
 		// arr 배열의 길이에 대해서
 		for(int i=0; i< arr.length; i++) {
 			// random함수, 즉 난수 랜덤발생기 함수 선언과 함께, 선언한 code의 길이와 더한다.
+			// 난수 랜덤 발생기 random함수 활용, length : 길이
 			int tmp = (int) (Math.random() + code.length);
 			arr[i] = code[tmp];
 		}
-		// 출력
+		// 출력, toString 활용
 		System.out.println(Arrays.toString(arr));
 	}
 }
