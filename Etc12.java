45e3533a68596d66ee541245271a3a3807d510ea
Jhchun95 <75196751+Jhchun95@users.noreply.github.com>
2022-02-19 23:26:48 +0900

    Add files via upload

---
+++ Etc12.java
@@ -0,0 +1,19 @@
+package mydolist;
+
+public class Etc12 {
+
+	public static void main(String[] args) {
+		
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		int d = 4;
+		
+		int e = a + b + c + d ;
+		// 저장을 해야 출력이 올바르게 된다.
+		// 코딩 다 해놓고 저장하지 않으면 출력이 안돼는 것은 당연
+		System.out.println(e);
+		
+	}
+
+}
