0648a9686d3a9f05078cbece1b9088a893dc52e5
Jhchun95 <75196751+Jhchun95@users.noreply.github.com>
2022-04-13 23:33:13 +0900

    Add files via upload

--- Exam15.java
+++ Exam15.java
@@ -3,10 +3,7 @@ package mydolist;
 public class Exam15 {
 
 	public static void main(String[] args) {
-		//week[] 배열 선언
 		String week[] = {"일","월","화","수","목","금", "토"};
-		// for ( : )문에 대해 익숙해지자
-		// w는 week 일때, 즉 일~토까지 순서대
 		for(String w : week)
 			System.out.print( w + "요일  ");	
 	}
