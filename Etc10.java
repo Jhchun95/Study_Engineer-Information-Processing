742bc41cb91bdfdb431cea2949d4b067483aab67
Jhchun95 <75196751+Jhchun95@users.noreply.github.com>
2022-04-14 23:50:20 +0900

    Update Etc10.java

--- Etc10.java
+++ Etc10.java
@@ -17,12 +17,32 @@ class Test2 extends Test{
 	}
 }
 
-public class Etc10 {
-	public static void main(String[] args) {
-		// 객체 선언
-		// 앞은 Test 클래스 A 이고, 뒤는 Test2 클래스이다. 변수 잘 보기
-		Test A = new Test2();
-		// Sample(2)의 함수값과 실행값을 잘 보도록 한다.
-		System.out.print(A.sample(2));
-	}
-}
+// 실제 문제
+// public class Etc10 {
+// 	public static void main(String[] args) {
+// 		// 객체 선언
+// 		// 앞은 Test 클래스 A 이고, 뒤는 Test2 클래스이다. 변수 잘 보기
+// 		Test A = new Test2();
+// 		// Sample(2)의 함수값과 실행값을 잘 보도록 한다.
+// 		System.out.print(A.sample(2));
+// 	}
+// }
+
+// class Parent {
+// 	void show() {
+//     	System.out.println("parent");
+//     }
+// }
+
+// class Child extends Parent {
+// 	void show() {
+//     	System.out.println("child");
+//     }
+// }
+
+// public class Test {
+// 	public static void main(String[] args) {
+//     	Parent pa = (①) Child();
+//         pa.show();
+//     }
+// }
